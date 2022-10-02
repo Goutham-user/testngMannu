074ec8d30045556cf588590e0c8b398f931504aa
goutham375 <goutham375@gmail.com>
2022-10-02 21:53:47 +0530

    testng commit

---
+++ src/test/java/com/example/TestngDemo.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class TestngDemo {
+  @Test
+  public void f() {
+	  System.out.println("test");
+	  System.setProperty("webdriver.chrome.driver", "D:\\testNg\\test\\testng\\webdrivers\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://google.com");
+		
+		driver.findElementByXPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").sendKeys("Hie Monika");
+		
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("before");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("after");
+  }
+
+}
